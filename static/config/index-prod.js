b35e8c29e305f55ebf90efb6d2f514739d8cef10
fengyafei <jiuyajava@163.com>
2021-02-01 17:24:19 +0800

    第二次, 服务端以部署

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -2,14 +2,13 @@
  * 生产环境
  */
 ;(function () {
-  window.SITE_CONFIG = {};
+  window.SITE_CONFIG = {}
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'https://api.jiousport.com/jos';
-  // window.SITE_CONFIG['baseUrl'] = 'http://123.56.228.251:8080/jos';
+  window.SITE_CONFIG['baseUrl'] = 'https://api.jiousport.com/seekpeople'
 
   // cdn地址 = 域名 + 版本号
-  window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '';   // 版本号(年月日时分)
-  window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
-})();
+  window.SITE_CONFIG['domain'] = './' // 域名
+  window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
+  window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version
+})()
--- static/config/index.js
+++ static/config/index.js
@@ -2,13 +2,14 @@
  * 开发环境
  */
 ;(function () {
-  window.SITE_CONFIG = {};
+  window.SITE_CONFIG = {}
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/jos';
+  window.SITE_CONFIG['baseUrl'] = 'https://127.0.0.1:8085/seekpeople'
+  // window.SITE_CONFIG['baseUrl'] = 'https://www.tianjinxinhuazhigong.cn:8085/seekpeople'
 
   // cdn地址 = 域名 + 版本号
-  window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '';   // 版本号(年月日时分)
-  window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
-})();
+  window.SITE_CONFIG['domain'] = './' // 域名
+  window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
+  window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version
+})()
