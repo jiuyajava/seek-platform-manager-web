0089e14a3516203d8f6445c35a437f79845d4ec5
fengyafei <jiuyajava@163.com>
2021-02-25 16:55:55 +0800

    update

--- src/views/modules/blackList/blackList.vue
+++ src/views/modules/blackList/blackList.vue
@@ -23,12 +23,6 @@
         width="50">
       </el-table-column>
       <el-table-column
-        prop="id"
-        header-align="center"
-        align="center"
-        label="">
-      </el-table-column>
-      <el-table-column
         prop="userId"
         header-align="center"
         align="center"
